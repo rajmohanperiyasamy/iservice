c9540690925f5f699bcac2b4454b522a4713b1be
rajmohanperiysamy <rajmohanjr@gmail.com>
2015-11-03 18:25:06 +0530

    url file

--- iservice/urls.py
+++ iservice/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'iservice.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
      # url(r'^blog/', include('blog.urls')),
+     # url(r'^blog/', include('blog.urls')),
 
 
     url(r'^admin/', include(admin.site.urls)),
