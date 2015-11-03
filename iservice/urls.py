e7685d458104bf43de1eb382486314e450ad4b4b
rajmohanperiysamy <rajmohanjr@gmail.com>
2015-11-03 18:52:29 +0530

    tomorrow is four

--- iservice/urls.py
+++ iservice/urls.py
@@ -6,8 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'iservice.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
      # url(r'^blog/', include('blog.urls')),
-     # url(r'^blog/', include('blog.urls')),
 
 
     url(r'^admin/', include(admin.site.urls)),
-)
+)  
+
+
+  #where
